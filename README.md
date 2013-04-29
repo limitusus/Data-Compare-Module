0edc01b7d4faeb3285503ac9877f9247be59db94
limitusus <limit.usus@gmail.com>
2013-04-30 03:01:21 +0900

    initial release

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Data::Compare::Module - Blah blah blah
+
+# SYNOPSIS
+
+    use Data::Compare::Module;
+
+# DESCRIPTION
+
+Data::Compare::Module is
+
+# AUTHOR
+
+Tomoya KABE <limitusus@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2013- Tomoya KABE
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Data/Compare/Module.pm
@@ -0,0 +1,39 @@
+package Data::Compare::Module;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::Compare::Module - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Data::Compare::Module;
+
+=head1 DESCRIPTION
+
+Data::Compare::Module is
+
+=head1 AUTHOR
+
+Tomoya KABE E<lt>limitusus@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Tomoya KABE
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
